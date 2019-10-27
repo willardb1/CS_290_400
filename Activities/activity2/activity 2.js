83f231351bd199c92903f366aa3e6630c2e64bc4
willardb1 <55929011+willardb1@users.noreply.github.com>
2019-10-27 13:36:40 -0700

    Create activity 2.js

---
+++ Activities/activity2/activity 2.js
@@ -0,0 +1,20 @@
+// Author: Brody Willard
+// Date: 10/9/19
+// Description: Activity 2: JS Enviroment
+console.log("Lets make sure JavaScript is working.");
+var name = "Brody"; //Replaced with my name 
+console.log("The unicode characters of your name are:")
+for (var i = 0; i < name.length; i++) {
+    console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.")
+
+//Result:
+//Lets make sure JavaScript is working.
+//The unicode characters of your name are:
+//66
+//114
+//111
+//100
+//121
+//Copy and paste these values for activity credit.
